577a2dd9461d939e535692b2855cdf8bca0fbb00
byutan <nguyenhthang1234@gmail.com>
2026-09-03 20:28:31 +0700

    Initial commit

---
+++ src/main/java/jp/co/frux/crud/eventbookingapp/EventBookingAppApplication.java
@@ -0,0 +1,13 @@
+package jp.co.frux.crud.eventbookingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventBookingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventBookingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jp/co/frux/crud/eventbookingapp/EventBookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.frux.crud.eventbookingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventBookingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
